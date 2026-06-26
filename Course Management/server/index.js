78de5b48901c66d53b8eed9e482613d2929c4828
孟浩然 <17198325+meng-han-han@user.noreply.gitee.com>
2026-06-26 12:24:21 +0800

    fix: use PORT env var for Railway deployment

--- Course Management/server/index.js
+++ Course Management/server/index.js
@@ -16,7 +16,7 @@ const { getPool, closePool } = require('./db');
 const { getCachedCourses, setCachedCourses, invalidateCourseCache } = require('./course-cache');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 // --------------- Middleware ---------------
 
